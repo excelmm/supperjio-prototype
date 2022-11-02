14cefd958395c42cc143e3e0c5c83abd204edada
Excel <excel.1999@yahoo.com>
2022-11-02 20:24:18 +0800

    first

--- app.py
+++ app.py
@@ -30,9 +30,6 @@ def respond():
     # the first time you chat with the bot AKA the welcoming message
     if text == "/start":
         # print the welcoming message
-     #    bot_welcome = """
-     #    Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-     #    """
         bot_welcome = "test"
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome,
@@ -42,13 +39,6 @@ def respond():
         try:
             # clear the message we got from any non alphabets
             text = re.sub(r"\W", "_", text)
-            # create the api link for the avatar based on http://avatars.adorable.io/
-            url = "https://api.adorable.io/avatars/285/{}.png".format(
-                text.strip())
-            # reply with a photo to the name the user sent,
-            # note that you can send photos by url and telegram will fetch it for you
-            bot.sendPhoto(chat_id=chat_id, photo=url,
-                          reply_to_message_id=msg_id)
         except Exception:
             # if things went wrong
             bot.sendMessage(
